2b87335f9f851bf09fa65e9e4f907c524a14cd2f
Nazar2015A <nazar.komar456@gmail.com>
2024-04-10 16:21:18 +0300

    add import useParams

--- front-end/src/pages/ResetPasswordPage/ResetPassword.jsx
+++ front-end/src/pages/ResetPasswordPage/ResetPassword.jsx
@@ -1,4 +1,5 @@
-import React, { useParams } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { motion } from "framer-motion";
 import {
